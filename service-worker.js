01fa616df4e83042c517cf2819172b66f084258c
novafusionmaster <novafusionmaster@gmail.com>
2025-08-08 03:40:48 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,41 @@
+const CACHE_NAME = 'nova-fusion-v1';
+const STATIC_ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './assets/icons/icon-192.png',
+  './assets/icons/icon-512.png',
+  './styles.css',
+  './app.js'
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('v1').then(cache =>
-      cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/Iara.jpg'
-      ])
-    )
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(STATIC_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(cacheResp => {
+        if (cacheResp) return cacheResp;
+        return fetch(event.request).then(resp =>
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, resp.clone());
+            return resp;
+          })
+        );
+      }).catch(() => caches.match('./offline.html'))
   );
 });
